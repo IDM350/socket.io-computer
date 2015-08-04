92302f0b0c7f695906b67ccebc202aef2adbbea8
IDM350 <M8R-2ful4v@spamgoeshere.com>
2015-08-04 19:41:23 -0400

    New turn model
    This not only prevents giving anyone more than one turn with a specially crafted client, as there's now a main model of single specific turns based on specific IDs, but also removes the database client and socket server from the scope of turn management with an event-based model, which also allows a separate module to control mouse and keyboard access on a server side which a specially crafted client (#8) can't bypass, and has been priorly designed to fit for a later feature.

--- turn.js
+++ turn.js
@@ -1,33 +1,92 @@
-var turnQueue = [];
-var activeTurn = false;
-var redis = require('./redis').emu();
+var Emitter = require('events').EventEmitter;
 
-var TURN_TIME = 15000;
+var TURN_TIMEOUT = 15;
 
-module.exports.push = function(sockid) {
-  turnQueue.push(sockid);
+function TurnRoster() {
+  if (!this instanceof TurnRoster) return new TurnRoster();
+  this.listings = {};
+  this.minorPositions = {};
+  this.scheduleIndex = [];
+  this.lastLeaseTime = 0;
 }
 
-module.exports.checkQueue = checkQueue;
-function checkQueue(newReq) {
-  var io = require('socket.io-emitter')(redis, {key: 'xpemu'});
+TurnRoster.prototype.__proto__ = Emitter.prototype;
 
-  if (!activeTurn && turnQueue.length >= 1) {
-    activeTurn = true;
-    var sockid = turnQueue.shift();
-    io = io.in(sockid);
-    io.emit('your-turn');
+TurnRoster.prototype.push = function(id) {
+  if (!this.listings[id]) this.listings[id] = new Date();
+}
 
-    setTimeout(function() {
-      io.emit('lose-turn');
-      activeTurn = false;
-      checkQueue(false);
-    }, TURN_TIME);
+TurnRoster.prototype.checkQueue = function() {
+  if (!Object.keys(this.listings).length) return;
 
-  } else if (newReq) {
-    var time = turnQueue.length * TURN_TIME;
-    var sockid = turnQueue[turnQueue.length - 1];
-    io = io.in(sockid);
-    io.emit('turn-ack', time);
+  function rebuildIndex() {
+    var orderedTimes = Object.keys(this.listings).map(function(id) {
+      return +this.listings[id];
+    }, this).sort();
+    var idRecalls = [];
+    this.scheduleIndex = new Array(Object.keys(this.listings).length);
+
+    for (id in this.listings) {
+      var firstIndex = orderedTimes.indexOf(this.listings[id]);
+      if (orderedTimes.indexOf(this.listings[id]), firstIndex + 1) > 0) {
+        if (this.minorPositions[id]) this.scheduleIndex[firstIndex + this.minorPositions[id] - 1] = id;
+        else idRecalls.push(id);
+      }
+      else {
+        this.scheduleIndex[firstIndex] = this.listings[firstIndex];
+      }
+    }
+
+    idRecalls.forEach(function(id) {
+      var minorPosition = 1, firstIndex = orderedTimes.indexOf(this.listings[id]);
+      while (this.scheduleIndex[firstIndex + minorPosition - 1]) minorPosition++;
+      this.scheduleIndex[firstIndex + minorPosition - 1] = id;
+      this.minorPositions[id] = minorPosition;
+    });
   }
+
+  var prevScheduleIndex = this.scheduleIndex;
+  if (+(new Date()) > this.lastLeaseTime + TURN_TIMEOUT) {
+    var idEpxirations = [];
+    if (prevScheduleIndex.length) {
+      var lastId = prevScheduleIndex[0];
+      if (this.minorPositions[lastId]) delete this.minorPositions[lastId];
+      delete this.listings[lastId];
+      idExpirations.push(lastId);
+    }
+    rebuildIndex();
+    if (this.scheduleIndex.length) {
+      this.emit('turn-lease', this.scheduleIndex[0]);
+      this.lastLeaseTime = +(new Date());
+      idExpirations.forEach(function(id) {
+        this.emit('turn-expired', id);
+      });
+    }
+  }
+  else if (Object.keys(this.listings).length > prevScheduleIndex.length) {
+    rebuildIndex();
+  }
+
+  this.scheduleIndex.slice(1).forEach(function(id, pos) {
+    if (prevScheduleIndex.indexOf(id) < 0) this.emit('turn-receipt', id, (+(new Date()) - this.lastLeaseTime) + (pos*TURN_TIMEOUT));
+  });
 }
+
+var redis = require('./redis').emu();
+var io = require('socket.io-emitter')(redis, {key: 'xpemu'});
+var socketCollection = {};
+
+module.exports = transitoryInstance = new TurnRoster();
+transitoryInstance.on('turn-lease', function(sockid) {
+  if (!socketCollection[sockid]) socketCollection[sockid] = io.in(sockid);
+  socketCollection[sockid].emit('your-turn');
+  setTimeout(transitoryInstance.checkQueue, TURN_TIMEOUT*1080);
+})
+transitoryInstance.on('turn-expired', function(sockid) {
+  socketCollection[sockid].emit('lose-turn');
+  delete socketCollection[sockid];
+});
+transitoryInstance.on('turn-receipt', function(sockid, time) {
+  socketCollection[sockid] = io.in(sockid);
+  socketCollection[sockid].emit('turn-ack', time*1000);
+});
